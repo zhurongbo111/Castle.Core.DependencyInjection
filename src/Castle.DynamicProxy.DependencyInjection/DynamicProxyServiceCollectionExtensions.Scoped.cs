9fea157af2475934a59e852aa2b0493b9336f9c4
Gary Zhu <390992476@qq.com>
2022-01-25 17:16:43 +0800

    update name with 'WithProxy' suffix

--- src/Castle.DynamicProxy.DependencyInjection/DynamicProxyServiceCollectionExtensions.Scoped.cs
+++ src/Castle.DynamicProxy.DependencyInjection/DynamicProxyServiceCollectionExtensions.Scoped.cs
@@ -6,41 +6,41 @@ namespace Castle.DynamicProxy.DependencyInjection
 {
     public static partial class DynamicProxyServiceCollectionExtensions
     {
-        public static IServiceCollection AddScoped(this IServiceCollection services, Type serviceType, Type implementationType, Action<ProxyServiceBuilder> proxySetup)
+        public static IServiceCollection AddScopedWithProxy(this IServiceCollection services, Type serviceType, Type implementationType, Action<ProxyServiceBuilder> proxySetup)
         {
             return services.AddScoped(serviceType, implementationType).EnableInterceptor(serviceType, proxySetup);
         }
 
-        public static IServiceCollection AddScoped<TService, TImplementation>(this IServiceCollection services, Action<ProxyServiceBuilder> proxySetup)
+        public static IServiceCollection AddScopedWithProxy<TService, TImplementation>(this IServiceCollection services, Action<ProxyServiceBuilder> proxySetup)
             where TService : class where TImplementation : class, TService
         {
-            return services.AddScoped(typeof(TService), typeof(TImplementation), proxySetup);
+            return services.AddScopedWithProxy(typeof(TService), typeof(TImplementation), proxySetup);
         }
 
-        public static IServiceCollection AddScoped(this IServiceCollection services, Type serviceType, Action<ProxyServiceBuilder> proxySetup)
+        public static IServiceCollection AddScopedWithProxy(this IServiceCollection services, Type serviceType, Action<ProxyServiceBuilder> proxySetup)
         {
-            return services.AddScoped(serviceType, serviceType, proxySetup);
+            return services.AddScopedWithProxy(serviceType, serviceType, proxySetup);
         }
 
-        public static IServiceCollection AddScoped<TService>(this IServiceCollection services, Action<ProxyServiceBuilder> proxySetup) where TService : class
+        public static IServiceCollection AddScopedWithProxy<TService>(this IServiceCollection services, Action<ProxyServiceBuilder> proxySetup) where TService : class
         {
-            return services.AddScoped(typeof(TService), proxySetup);
+            return services.AddScopedWithProxy(typeof(TService), proxySetup);
         }
 
-        public static IServiceCollection AddScoped(this IServiceCollection services, Type serviceType, Func<IServiceProvider, object> implementationFactory, Action<ProxyServiceBuilder> proxySetup)
+        public static IServiceCollection AddScopedWithProxy(this IServiceCollection services, Type serviceType, Func<IServiceProvider, object> implementationFactory, Action<ProxyServiceBuilder> proxySetup)
         {
             return services.AddScoped(serviceType, implementationFactory).EnableInterceptor(serviceType, proxySetup);
         }
 
-        public static IServiceCollection AddScoped<TService>(this IServiceCollection services, Func<IServiceProvider, TService> implementationFactory, Action<ProxyServiceBuilder> proxySetup) where TService : class
+        public static IServiceCollection AddScopedWithProxy<TService>(this IServiceCollection services, Func<IServiceProvider, TService> implementationFactory, Action<ProxyServiceBuilder> proxySetup) where TService : class
         {
-            return services.AddScoped(typeof(TService), implementationFactory, proxySetup);
+            return services.AddScopedWithProxy(typeof(TService), implementationFactory, proxySetup);
         }
 
-        public static IServiceCollection AddScoped<TService, TImplementation>(this IServiceCollection services, Func<IServiceProvider, TImplementation> implementationFactory, Action<ProxyServiceBuilder> proxySetup)
+        public static IServiceCollection AddScopedWithProxy<TService, TImplementation>(this IServiceCollection services, Func<IServiceProvider, TImplementation> implementationFactory, Action<ProxyServiceBuilder> proxySetup)
             where TService : class where TImplementation : class, TService
         {
-            return services.AddScoped(typeof(TService), implementationFactory, proxySetup);
+            return services.AddScopedWithProxy(typeof(TService), implementationFactory, proxySetup);
         }
     }
 }
--- src/Castle.DynamicProxy.DependencyInjection/DynamicProxyServiceCollectionExtensions.Singleton.cs
+++ src/Castle.DynamicProxy.DependencyInjection/DynamicProxyServiceCollectionExtensions.Singleton.cs
@@ -6,57 +6,57 @@ namespace Castle.DynamicProxy.DependencyInjection
 {
     public static partial class DynamicProxyServiceCollectionExtensions
     {
-        public static IServiceCollection AddSingleton(this IServiceCollection services, Type serviceType, Type implementationType, Action<ProxyServiceBuilder> proxySetup)
+        public static IServiceCollection AddSingletonWithProxy(this IServiceCollection services, Type serviceType, Type implementationType, Action<ProxyServiceBuilder> proxySetup)
         {
             return services.AddSingleton(serviceType, implementationType).EnableInterceptor(serviceType, proxySetup);
         }
 
-        public static IServiceCollection AddSingleton<TService, TImplementation>(this IServiceCollection services, Action<ProxyServiceBuilder> proxySetup)
+        public static IServiceCollection AddSingletonWithProxy<TService, TImplementation>(this IServiceCollection services, Action<ProxyServiceBuilder> proxySetup)
             where TService : class where TImplementation : class, TService
         {
 
-            return services.AddSingleton(typeof(TService), typeof(TImplementation), proxySetup);
+            return services.AddSingletonWithProxy(typeof(TService), typeof(TImplementation), proxySetup);
         }
 
-        public static IServiceCollection AddSingleton(this IServiceCollection services, Type serviceType, Action<ProxyServiceBuilder> proxySetup)
+        public static IServiceCollection AddSingletonWithProxy(this IServiceCollection services, Type serviceType, Action<ProxyServiceBuilder> proxySetup)
         {
-            return services.AddSingleton(serviceType, serviceType, proxySetup);
+            return services.AddSingletonWithProxy(serviceType, serviceType, proxySetup);
         }
 
-        public static IServiceCollection AddSingleton<TService>(this IServiceCollection services, Action<ProxyServiceBuilder> proxySetup)
+        public static IServiceCollection AddSingletonWithProxy<TService>(this IServiceCollection services, Action<ProxyServiceBuilder> proxySetup)
             where TService : class
         {
-            return services.AddSingleton(typeof(TService), proxySetup);
+            return services.AddSingletonWithProxy(typeof(TService), proxySetup);
         }
 
-        public static IServiceCollection AddSingleton(this IServiceCollection services, Type serviceType, Func<IServiceProvider, object> implementationFactory, Action<ProxyServiceBuilder> proxySetup)
+        public static IServiceCollection AddSingletonWithProxy(this IServiceCollection services, Type serviceType, Func<IServiceProvider, object> implementationFactory, Action<ProxyServiceBuilder> proxySetup)
         {
 
             return services.AddSingleton(serviceType, implementationFactory).EnableInterceptor(serviceType, proxySetup);
         }
 
-        public static IServiceCollection AddSingleton<TService>(this IServiceCollection services, Func<IServiceProvider, TService> implementationFactory, Action<ProxyServiceBuilder> proxySetup)
+        public static IServiceCollection AddSingletonWithProxy<TService>(this IServiceCollection services, Func<IServiceProvider, TService> implementationFactory, Action<ProxyServiceBuilder> proxySetup)
             where TService : class
         {
-            return services.AddSingleton(typeof(TService), implementationFactory, proxySetup);
+            return services.AddSingletonWithProxy(typeof(TService), implementationFactory, proxySetup);
         }
 
-        public static IServiceCollection AddSingleton<TService, TImplementation>(this IServiceCollection services, Func<IServiceProvider, TImplementation> implementationFactory, Action<ProxyServiceBuilder> proxySetup)
+        public static IServiceCollection AddSingletonWithProxy<TService, TImplementation>(this IServiceCollection services, Func<IServiceProvider, TImplementation> implementationFactory, Action<ProxyServiceBuilder> proxySetup)
             where TService : class where TImplementation : class, TService
         {
 
-            return services.AddSingleton(typeof(TService), implementationFactory, proxySetup);
+            return services.AddSingletonWithProxy(typeof(TService), implementationFactory, proxySetup);
         }
 
-        public static IServiceCollection AddSingleton(this IServiceCollection services, Type serviceType, object implementationInstance, Action<ProxyServiceBuilder> proxySetup)
+        public static IServiceCollection AddSingletonWithProxy(this IServiceCollection services, Type serviceType, object implementationInstance, Action<ProxyServiceBuilder> proxySetup)
         {
             return services.AddSingleton(serviceType, implementationInstance).EnableInterceptor(serviceType, proxySetup);
         }
 
-        public static IServiceCollection AddSingleton<TService>(this IServiceCollection services, TService implementationInstance, Action<ProxyServiceBuilder> proxySetup)
+        public static IServiceCollection AddSingletonWithProxy<TService>(this IServiceCollection services, TService implementationInstance, Action<ProxyServiceBuilder> proxySetup)
             where TService : class
         {
-            return services.AddSingleton(typeof(TService), implementationInstance, proxySetup);
+            return services.AddSingletonWithProxy(typeof(TService), implementationInstance, proxySetup);
         }
     }
 }

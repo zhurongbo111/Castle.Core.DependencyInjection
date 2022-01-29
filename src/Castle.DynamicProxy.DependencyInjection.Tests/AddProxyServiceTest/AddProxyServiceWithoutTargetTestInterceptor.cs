97131ebfcadee449dbb9ef34bcb3f6bcedbfdb38
Gary Zhu <390992476@qq.com>
2022-01-29 16:51:35 +0800

    add unit test for AddProxyServiceWithoutTarget

---
+++ src/Castle.DynamicProxy.DependencyInjection.Tests/AddProxyServiceTest/AddProxyServiceWithoutTargetTestInterceptor.cs
@@ -0,0 +1,12 @@
+﻿namespace Castle.DynamicProxy.DependencyInjection.Tests
+{
+    internal class AddProxyServiceWithoutTargetTestInterceptor : IInterceptor
+    {
+        public static int CalledCount = 0;
+
+        public void Intercept(IInvocation invocation)
+        {
+            CalledCount++;
+        }
+    }
+}
